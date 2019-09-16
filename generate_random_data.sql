3c2bc459b09d35dba4cb0453407c4e9ee052f9fd
cmennens <carlos.mennens@gmail.com>
2019-09-16 11:43:39 -0400

    Changed column alias name.

--- generate_random_data.sql
+++ generate_random_data.sql
@@ -1,1 +1,1 @@
-SELECT generate_series(1,10) AS id, md5(random()::text) AS descr; -- generate random pg data
+SELECT generate_series(1,10) AS id, md5(random()::text) AS test_col; -- generate random pg data
