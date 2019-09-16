242fb3aaf4d676c4ccd54015a41e008b170e3f2e
cmennens <carlos.mennens@gmail.com>
2019-09-16 16:12:34 -0400

    Adding sequences help file.

---
+++ sequences.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE seq_meh_id;
+SELECT setval('seq_meh_id', MAX(id)) FROM meh; 
+
+ALTER TABLE meh ALTER COLUMN id SET DEFAULT
+nextval('seq_meh_id');
+
+-- # PostgreSQL documentation URL:
+-- http://www.postgresql.org/docs/9.4/static/datatype-numeric.html#DATATYPE-SERIAL
+
+--===================VERSION 2.0=====================
+
+CREATE vendor_seq start 7000;
+
+CREATE TABLE vendor 
+(
+    vendor_id int primary key default nextval('vendor_seq'),
+    vendor_name varchar(50) unique not null
+);
+
+-- And to reset the current value of the sequence:
+ALTER SEQUENCE vendor_seq restart 8000;
