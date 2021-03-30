481e51766fb7d3c893ed53b6bc27f1b66e0cf98d
Carlos Mennens <cmennens@users.noreply.github.com>
2021-03-30 19:31:04 -0400

    Update generate_random_data.sql

--- generate_random_data.sql
+++ generate_random_data.sql
@@ -1,1 +1,4 @@
-SELECT generate_series(1,10) AS id, md5(random()::text) AS test_col; -- generate random pg data
+--generate random data in PostgreSQL
+SELECT 
+  generate_series(1,10) AS id, 
+  md5(random()::text) AS test_col;
